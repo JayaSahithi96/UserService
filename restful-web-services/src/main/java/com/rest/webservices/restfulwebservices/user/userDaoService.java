fe6723ab852700f892a9b74b7507c7ba8f73d359
sahithi31 <jsahithi90@gmail.com>
2021-06-10 17:26:48 -0700

    Exception Handling

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/userDaoService.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/userDaoService.java
@@ -3,6 +3,7 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -36,5 +37,18 @@ public class userDaoService {
 		}
 		return null;
 	}
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		
+		while(iterator.hasNext()) {
+			User user=iterator.next();
+			if(user.getId()==id) {
+				iterator.remove(); 
+				return user;
+			}
+		}
+		return null;
+	}
+	
 	
 }
